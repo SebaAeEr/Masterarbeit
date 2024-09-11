e118b744fc4fc1fcd5ffed5fef5961f4302a9bad
SebaAeEr <s.erasmus@gmx.de>
2024-09-11 22:29:56 +0200

    asdf

--- logs/Analyse.py
+++ logs/Analyse.py
@@ -14,11 +14,45 @@ def printEingerückt(string: str, tabs: int):
         space += "\t"
     print(space + string)
 
+
+def transTimes(time):
+    print(time)
+    res = 0
+    if time[-1] == "d":
+        res = float(time[:-1]) * 24 * 60
+    elif time[-1] == "h":
+        res = float(time[:-2]) * 60
+    elif time[-1] == "m":
+        res = float(time[:-1])
+    elif time[-2:] == "ms":
+        res = float(time[:-2]) / (60 * 1000)
+    elif time[-2:] == "ns":
+        res = float(time[:-2]) / (60 * 1000000)
+    elif time[-2:] == "us":
+        res = float(time[:-2]) / (60 * 1000000000)
+    elif time[-1] == "s":
+        res = float(time[:-1]) / 60
+    print(res)
+    return res
+
+
 def CollectOPData(i, tabs):
-    walltemp = i["blockedWall"]
-    printEingerückt("Wall time: " + walltemp, tabs)
+    blockedwall = i["blockedWall"]
+    printEingerückt("Wall time: " + blockedwall, tabs)
     spilltemp = convertByteToGB(i["spilledDataSize"])
     printEingerückt("Spill Data: " + str(spilltemp) + " GB", tabs)
+    cpu_time = (
+        transTimes(i["addInputCpu"])
+        + transTimes(i["getOutputCpu"])
+        + transTimes(i["finishCpu"])
+    )
+    wall_time = (
+        transTimes(i["addInputWall"])
+        + transTimes(i["getOutputWall"])
+        + transTimes(i["finishWall"])
+    )
+    return max(transTimes(blockedwall), wall_time), cpu_time, spilltemp
+
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -191,16 +225,53 @@ def JoinOtherTable():
     plt.show()
 
 
-
 def TPC():
-    qspillSize = np.empty([3, 4], dtype=float)
-    qexecTime = np.empty([3, 4], dtype=float)
-    hspillSize = np.empty([3, 4], dtype=float)
-    hexecTime = np.empty([3, 4], dtype=float)
+    spilledData = []
+    query_times = []
+    query_names = []
+    query_p_user_mem = []
+    query_joins = []
+    query_aggs = []
+    query_join_spills = []
+    query_agg_spills = []
+    query_hash_spills = []
+    query_times_join = []
+    query_times_agg = []
+    query_times_ex = []
+    query_times_hash = []
+    query_times_cpu_join = []
+    query_times_cpu_agg = []
+    query_times_cpu_ex = []
+    query_times_cpu_hash = []
+    query_all_time = []
+    query_all_time_cpu = []
+    all_join_spills = []
+    all_join_times = []
+    all_join_times_cpu = []
+    all_hash_spills = []
+    all_hash_times = []
+    all_hash_times_cpu = []
+    all_agg_spills = []
+    all_agg_times = []
+    all_agg_times_cpu = []
     memcounter = 0
     tabs = 0
     for k in range(1, 23):
-        threshcounter = 0
+        join_spills = 0
+        agg_spills = 0
+        hash_spills = 0
+        join_counter = 0
+        agg_counter = 0
+        join_time = 0
+        agg_time = 0
+        ex_time = 0
+        hash_time = 0
+        all_time = 0
+        join_time_cpu = 0
+        agg_time_cpu = 0
+        ex_time_cpu = 0
+        hash_time_cpu = 0
+        all_time_cpu = 0
         tabs = 0
         printEingerückt(str(k) + ":", tabs)
         tabs += 1
@@ -211,68 +282,208 @@ def TPC():
         except:
             print("File " + name + " not found.")
             continue
+        query_names.append("Query " + str(k))
         data = json.load(f)
 
-        qTime = data["queryStats"]["executionTime"]
-        printEingerückt("ExecutionTime: " + qTime, tabs)
+        qTime = transTimes(data["queryStats"]["executionTime"])
+        printEingerückt("ExecutionTime: " + str(qTime), tabs)
+        query_times.append(qTime)
 
         qData = convertByteToGB(data["queryStats"]["spilledDataSize"])
         printEingerückt("Spilled Data: " + str(qData) + " GB", tabs)
-        #qspillSize[memcounter][threshcounter] = qData
-        #qexecTime[memcounter][threshcounter] = float(qTime[:-1])
+        spilledData.append(qData)
+
+        p_user_mem = convertByteToGB(data["queryStats"]["peakUserMemoryReservation"])
+        printEingerückt("Peak User Memory: " + str(p_user_mem) + " GB", tabs)
+        query_p_user_mem.append(p_user_mem)
+        # qspillSize[memcounter][threshcounter] = qData
+        # qexecTime[memcounter][threshcounter] = float(qTime[:-1])
         for i in data["queryStats"]["operatorSummaries"]:
             if i["operatorType"] == "HashBuilderOperator":
                 printEingerückt("Hash:", tabs)
-                CollectOPData(i, tabs + 1)
-                #if walltemp[-1] == "d":
-               #     time =  float(walltemp[:-1]) * 24 * 60
-              #  elif walltemp[-1] == "h":
-             #       time =  float(walltemp[:-2]) * 60
-            #    elif walltemp[-1] == "m":
-                #    time = float(walltemp[:-1])
-               # elif walltemp[-2] == "ms":
-              #      time =  float(walltemp[:-2]) / 3600
-             #   elif walltemp[-2] == "ns":
-            #        time =  float(walltemp[:-2]) / (60 * 60 * 60)
-           #     elif walltemp[-1] == "s":
-          #          time =  float(walltemp[:-1]) / 60
-                #hexecTime[memcounter][threshcounter] = time
+                wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                hash_spills += spill
+                hash_time += wtime
+                hash_time_cpu += cputime
+                all_hash_spills.append(spill)
+                all_hash_times.append(wtime)
+                all_hash_times_cpu.append(cputime)
+                join_counter += 1
             elif i["operatorType"] == "LookupJoinOperator":
                 printEingerückt("Join:", tabs)
-                CollectOPData(i, tabs + 1)
+                wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                join_spills += spill
+                join_time += wtime
+                join_time_cpu += cputime
+                all_join_spills.append(spill)
+                all_join_times.append(wtime)
+                all_join_times_cpu.append(cputime)
+                join_counter += 1
             elif i["operatorType"] == "HashAggregationOperator":
                 printEingerückt("HashAgg:", tabs)
-                CollectOPData(i, tabs + 1)
+                wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                agg_spills += spill
+                agg_time += wtime
+                agg_time_cpu += cputime
+                all_agg_spills.append(spill)
+                all_agg_times.append(wtime)
+                all_agg_times_cpu.append(cputime)
+                agg_counter += 1
             elif i["operatorType"] == "AggregationOperator":
                 printEingerückt("Agg:", tabs)
-                CollectOPData(i, tabs + 1) 
+                wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                agg_spills += spill
+                agg_time += wtime
+                agg_time_cpu += cputime
+                all_agg_spills.append(spill)
+                all_agg_times.append(wtime)
+                all_agg_times_cpu.append(cputime)
+                agg_counter += 1
+            elif "Exchange" in i["operatorType"]:
+                printEingerückt("Exchange:", tabs)
+                wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                ex_time += wtime
+                ex_time_cpu += cputime
+            else:
+                wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                all_time += wtime
+                all_time_cpu += cputime
         f.close()
-        threshcounter += 1
         tabs -= 1
+        query_aggs.append(agg_counter)
+        query_joins.append(join_counter)
+        query_agg_spills.append(agg_spills)
+        query_join_spills.append(join_spills)
+        query_hash_spills.append(hash_spills)
+        query_times_join.append(join_time)
+        query_times_agg.append(agg_time)
+        query_times_ex.append(ex_time)
+        query_times_hash.append(hash_time)
+        query_all_time.append(all_time)
+        query_times_cpu_join.append(join_time_cpu)
+        query_times_cpu_agg.append(agg_time_cpu)
+        query_times_cpu_ex.append(ex_time_cpu)
+        query_times_cpu_hash.append(hash_time_cpu)
+        query_all_time_cpu.append(all_time_cpu)
+
     memcounter += 1
 
-    x = 0
-    colors = np.array(["r", "g", "b"])
-    labels = np.array(["2,8", "3,5", "4,2"])
-    fig, axis = plt.subplots(1, 2)
-    for i in hspillSize:
-        axis[0].plot(i, hexecTime[x], color=colors[x], label=labels[x], marker="o")
-        axis[1].plot(
-            qspillSize[x], qexecTime[x], color=colors[x], label=labels[x], marker="o"
-        )
-        x += 1
-    axis[0].set_xlabel("Spilled Datasize of the Query in GB")
-    axis[0].set_ylabel("Query Execution Time")
-    axis[0].set_title("Query execution time dependence of amount of spilled data")
-    axis[0].grid(visible=True)
-    axis[0].legend()
-    axis[1].set_xlabel("Spilled Datasize of the Hash Operator in GB")
-    axis[1].set_ylabel("Blocked Wall Time of the Hash Operator")
-    axis[1].set_title(
-        "Hash Operation execution time dependence of amount of spilled data"
+    fig, ax = plt.subplots()
+    x = np.arange(len(query_names))
+    width = 0.25  # the width of the bars
+    rects = ax.bar(x, query_p_user_mem, width, label="Peak User Memory")
+    ax.bar_label(rects, padding=2)
+
+    other = np.subtract(
+        spilledData,
+        np.add(np.add(query_agg_spills, query_join_spills), query_hash_spills),
     )
-    axis[1].grid(visible=True)
-    axis[1].legend()
+    spills = {
+        "Join": query_join_spills,
+        "Aggregation": query_agg_spills,
+        "Hash": query_hash_spills,
+        "Other": other,
+    }
+    bottom = np.zeros(len(query_join_spills))
+    for label, data in spills.items():
+        rects = ax.bar(x + width, data, width, label=label, bottom=bottom)
+        bottom += data
+        ax.bar_label(rects, padding=2)
+
+    ax.set_xticks(x + width / 2)
+    ax.set_xticklabels(query_names)
+    ax.legend(loc="upper right")
+    ax.set_ylabel("Spilled Data in GB")
     plt.show()
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(query_times, spilledData)
+    for i, txt in enumerate(query_names):
+        ax.annotate(txt, (query_times[i], spilledData[i]))
+    ax.set_xlabel("Wall time of Query in min")
+    ax.set_ylabel("Data spilled in GB")
+    plt.show()
+
+    fig, ax = plt.subplots()
+    op_names = ["Joins", "Aggregations"]
+    rects = ax.bar(x, query_joins, width, label="Joins")
+    ax.bar_label(rects, padding=2)
+    rects = ax.bar(x + width, query_aggs, width, label="Aggregations")
+    ax.bar_label(rects, padding=2)
+    ax.set_xticks(x + width / 2)
+    ax.set_xticklabels(query_names)
+    ax.legend(loc="upper right")
+    ax.set_ylabel("Number of Operations in Query")
+    plt.show()
+
+    fig, ax = plt.subplots(1, 2)
+    ax21 = ax[0]
+
+    other_times = np.subtract(
+        query_times,
+        np.add(
+            np.add(np.add(query_times_join, query_times_agg), query_times_ex),
+            query_times_hash,
+        ),
+    )
+    times = {
+        "Join": query_times_join,
+        "Aggregation": query_times_agg,
+        # "Exchange": query_times_ex,
+        "Hash": query_times_hash,
+        # "Other": all_time,
+    }
+    bottom = np.zeros(len(query_join_spills))
+    for label, data in times.items():
+        rects = ax21.bar(query_names, data, width, label=label, bottom=bottom)
+        bottom += data
+        ax21.bar_label(rects, padding=2)
+    ax21.legend(loc="upper right")
+    ax21.set_ylabel("Wall time of Query in min")
+    ax22 = ax[1]
+
+    other_times = np.subtract(
+        query_times,
+        np.add(
+            np.add(np.add(query_times_join, query_times_agg), query_times_ex),
+            query_times_hash,
+        ),
+    )
+    times = {
+        "Join": query_times_cpu_join,
+        "Aggregation": query_times_cpu_agg,
+        # "Exchange": query_times_ex,
+        "Hash": query_times_cpu_hash,
+        # "Other": all_time,
+    }
+    bottom = np.zeros(len(query_join_spills))
+    for label, data in times.items():
+        rects = ax22.bar(query_names, data, width, label=label, bottom=bottom)
+        bottom += data
+        ax22.bar_label(rects, padding=2)
+    ax22.legend(loc="upper right")
+    ax22.set_ylabel("CPU time of Query in min")
+    plt.show()
+
+    fig, ax = plt.subplots(1, 2)
+    ax31 = ax[0]
+    ax31.scatter(all_hash_times, all_hash_spills, marker=".", label="Hash")
+    ax31.scatter(all_join_times, all_join_spills, marker="s", label="Join")
+    ax31.scatter(all_agg_times, all_agg_spills, marker="^", label="Aggregation")
+    ax31.legend(loc="upper right")
+    ax31.set_ylabel("Wall time of Operation in min")
+    ax31.set_xlabel("Data spilled by Operation in GB")
+
+    ax32 = ax[1]
+    ax32.scatter(all_hash_times_cpu, all_hash_spills, marker=".", label="Hash")
+    ax32.scatter(all_join_times_cpu, all_join_spills, marker="s", label="Join")
+    ax32.scatter(all_agg_times_cpu, all_agg_spills, marker="^", label="Aggregation")
+    ax32.legend(loc="upper right")
+    ax32.set_ylabel("CPU time of Operation in min")
+    ax32.set_xlabel("Data spilled by Operation in GB")
+
+    plt.show()
+
 
 TPC()
