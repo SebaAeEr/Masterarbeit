d03c54666f76d94c823147c539b759cb3d00c733
SebaAeEr <s.erasmus@gmx.de>
2024-09-20 14:47:04 +0200

    asdf

--- logs/Analyse.py
+++ logs/Analyse.py
@@ -16,12 +16,11 @@ def printEingerückt(string: str, tabs: int):
 
 
 def transTimes(time):
-    print(time)
     res = 0
     if time[-1] == "d":
         res = float(time[:-1]) * 24 * 60
     elif time[-1] == "h":
-        res = float(time[:-2]) * 60
+        res = float(time[:-1]) * 60
     elif time[-1] == "m":
         res = float(time[:-1])
     elif time[-2:] == "ms":
@@ -32,7 +31,6 @@ def transTimes(time):
         res = float(time[:-2]) / (60 * 1000000000)
     elif time[-1] == "s":
         res = float(time[:-1]) / 60
-    print(res)
     return res
 
 
@@ -51,7 +49,7 @@ def CollectOPData(i, tabs):
         + transTimes(i["getOutputWall"])
         + transTimes(i["finishWall"])
     )
-    return max(transTimes(blockedwall), wall_time), cpu_time, spilltemp
+    return max(wall_time, transTimes(blockedwall)), cpu_time, spilltemp
 
 
 abspath = os.path.abspath(__file__)
@@ -281,7 +279,12 @@ def TPC():
             f = open(os.path.join(directory, name))
         except:
             print("File " + name + " not found.")
-            continue
+            try:
+                name = "tpc_" + str(k) + "(failed).json"
+                f = open(os.path.join(directory, name))
+            except:
+                print("failed to open failed!")
+                continue
         query_names.append("Query " + str(k))
         data = json.load(f)
 
@@ -299,8 +302,8 @@ def TPC():
         # qspillSize[memcounter][threshcounter] = qData
         # qexecTime[memcounter][threshcounter] = float(qTime[:-1])
         for i in data["queryStats"]["operatorSummaries"]:
+            printEingerückt(i["operatorType"], tabs)
             if i["operatorType"] == "HashBuilderOperator":
-                printEingerückt("Hash:", tabs)
                 wtime, cputime, spill = CollectOPData(i, tabs + 1)
                 hash_spills += spill
                 hash_time += wtime
@@ -310,7 +313,6 @@ def TPC():
                 all_hash_times_cpu.append(cputime)
                 join_counter += 1
             elif i["operatorType"] == "LookupJoinOperator":
-                printEingerückt("Join:", tabs)
                 wtime, cputime, spill = CollectOPData(i, tabs + 1)
                 join_spills += spill
                 join_time += wtime
@@ -320,7 +322,6 @@ def TPC():
                 all_join_times_cpu.append(cputime)
                 join_counter += 1
             elif i["operatorType"] == "HashAggregationOperator":
-                printEingerückt("HashAgg:", tabs)
                 wtime, cputime, spill = CollectOPData(i, tabs + 1)
                 agg_spills += spill
                 agg_time += wtime
@@ -330,7 +331,6 @@ def TPC():
                 all_agg_times_cpu.append(cputime)
                 agg_counter += 1
             elif i["operatorType"] == "AggregationOperator":
-                printEingerückt("Agg:", tabs)
                 wtime, cputime, spill = CollectOPData(i, tabs + 1)
                 agg_spills += spill
                 agg_time += wtime
@@ -340,7 +340,6 @@ def TPC():
                 all_agg_times_cpu.append(cputime)
                 agg_counter += 1
             elif "Exchange" in i["operatorType"]:
-                printEingerückt("Exchange:", tabs)
                 wtime, cputime, spill = CollectOPData(i, tabs + 1)
                 ex_time += wtime
                 ex_time_cpu += cputime
@@ -370,7 +369,7 @@ def TPC():
 
     fig, ax = plt.subplots()
     x = np.arange(len(query_names))
-    width = 0.25  # the width of the bars
+    width = 0.4  # the width of the bars
     rects = ax.bar(x, query_p_user_mem, width, label="Peak User Memory")
     ax.bar_label(rects, padding=2)
 
@@ -468,22 +467,207 @@ def TPC():
 
     fig, ax = plt.subplots(1, 2)
     ax31 = ax[0]
-    ax31.scatter(all_hash_times, all_hash_spills, marker=".", label="Hash")
-    ax31.scatter(all_join_times, all_join_spills, marker="s", label="Join")
-    ax31.scatter(all_agg_times, all_agg_spills, marker="^", label="Aggregation")
+    ax31.scatter(all_hash_times, all_hash_spills, marker="o", label="Hash", s=80)
+    ax31.scatter(all_join_times, all_join_spills, marker="s", label="Join", s=80)
+    ax31.scatter(all_agg_times, all_agg_spills, marker="^", label="Aggregation", s=80)
     ax31.legend(loc="upper right")
-    ax31.set_ylabel("Wall time of Operation in min")
-    ax31.set_xlabel("Data spilled by Operation in GB")
+    ax31.set_xlabel("Wall time of Operation in min")
+    ax31.set_ylabel("Data spilled by Operation in GB")
 
     ax32 = ax[1]
-    ax32.scatter(all_hash_times_cpu, all_hash_spills, marker=".", label="Hash")
-    ax32.scatter(all_join_times_cpu, all_join_spills, marker="s", label="Join")
-    ax32.scatter(all_agg_times_cpu, all_agg_spills, marker="^", label="Aggregation")
+    ax32.scatter(all_hash_times_cpu, all_hash_spills, marker="o", label="Hash", s=80)
+    ax32.scatter(all_join_times_cpu, all_join_spills, marker="s", label="Join", s=80)
+    ax32.scatter(
+        all_agg_times_cpu, all_agg_spills, marker="^", label="Aggregation", s=80
+    )
     ax32.legend(loc="upper right")
-    ax32.set_ylabel("CPU time of Operation in min")
-    ax32.set_xlabel("Data spilled by Operation in GB")
+    ax32.set_xlabel("CPU time of Operation in min")
+    ax32.set_ylabel("Data spilled by Operation in GB")
 
     plt.show()
 
 
+def analyse_Query(number):
+    all_hash_times = []
+    all_hash_spills = []
+    all_hash_times_cpu = []
+    hash_names = []
+    all_wtimes = []
+    all_cputimes = []
+    all_names = []
+    all_wtimes_operators = {}
+    all_cputimes_operators = {}
+
+    name = "tpc_" + number + ".json"
+    try:
+        directory = "tpc"
+        f = open(os.path.join(directory, name))
+    except:
+        print("File " + name + " not found.")
+    data = json.load(f)
+    for i in data["queryStats"]["operatorSummaries"]:
+        wtime, cputime, spill = CollectOPData(i, 0)
+        # if wtime > 0:
+        if i["operatorType"][:-8] in all_wtimes_operators:
+            all_wtimes_operators[i["operatorType"][:-8]] += wtime
+            all_cputimes_operators[i["operatorType"][:-8]] += cputime
+        else:
+            all_wtimes_operators[i["operatorType"][:-8]] = wtime
+            all_cputimes_operators[i["operatorType"][:-8]] = cputime
+        all_names.append(i["operatorType"][:-8] + " " + str(i["stageId"]))
+        all_wtimes.append(wtime)
+        all_cputimes.append(cputime)
+        if i["operatorType"] == "HashBuilderOperator":
+            all_hash_spills.append(spill)
+            all_hash_times.append(wtime)
+            all_hash_times_cpu.append(cputime)
+            hash_names.append(i["stageId"])
+
+    fig, ax = plt.subplots()
+    width = 0.25
+    x = np.arange(len(hash_names))
+    rects = ax.bar(hash_names, all_hash_spills, width)
+    ax.legend(loc="upper right")
+    ax.set_xlabel("CPU time of Operation in min")
+    ax.set_ylabel("Data spilled by Operation in GB")
+    ax.bar_label(rects)
+    plt.show()
+
+    # fig, ax = plt.subplots(2, 1)
+    # width = 0.25
+    # ax1 = ax[0]
+    # data = list(zip(all_names, all_wtimes))
+    # sorted_data = sorted(data, key=lambda item: item[1], reverse=True)
+    # x_sorted, y_sorted = zip(*sorted_data)
+    # rects = ax1.bar(x_sorted, y_sorted, width)
+    # ax1.legend(loc="upper right")
+    # ax1.set_ylabel("Wall time of Operation in min")
+    # ax1.bar_label(rects)
+    # width = 0.25
+    # ax2 = ax[1]
+    # data = list(zip(all_names, all_cputimes))
+    # sorted_data = sorted(data, key=lambda item: item[1], reverse=True)
+    # x_sorted, y_sorted = zip(*sorted_data)
+    # rects = ax2.bar(x_sorted, y_sorted, width)
+    # ax2.legend(loc="upper right")
+    # ax2.set_ylabel("CPU time of Operation in min")
+    # ax2.bar_label(rects)
+
+    fig, ax = plt.subplots(2, 1)
+    width = 0.25
+    ax1 = ax[0]
+    wtimes_temp = list(all_wtimes_operators.values())
+    names_temp = list(all_wtimes_operators.keys())
+    data = list(zip(names_temp, wtimes_temp))
+    sorted_data = sorted(data, key=lambda item: item[0], reverse=True)
+    x_sorted, y_sorted = zip(*sorted_data)
+    rects = ax1.bar(x_sorted, y_sorted, width)
+    ax1.set_ylabel("Wall time of Operation in min")
+    ax1.bar_label(rects)
+
+    cputimes_temp = list(all_cputimes_operators.values())
+    names_temp = list(all_cputimes_operators.keys())
+    ax2 = ax[1]
+    data = list(zip(names_temp, cputimes_temp))
+    sorted_data = sorted(data, key=lambda item: item[0], reverse=True)
+    x_sorted, y_sorted = zip(*sorted_data)
+    rects = ax2.bar(x_sorted, y_sorted, width)
+    ax2.set_ylabel("CPU time of Operation in min")
+    ax2.bar_label(rects)
+
+    plt.show()
+
+
+def analyse_1_6_13():
+    memcounter = 0
+    tabs = 0
+    query_ids = [1, 6, 13]
+    data_scale = [100, 300, 1000]
+    worker_size = [4, 5, 6]
+    query_names = []
+    query_times = np.zeros((3, 3, 3), float)
+    spilledData = np.zeros((3, 3, 3), float)
+    query_p_user_mem = [[[]]]
+    for qid in range(3):
+        query_names.append("Query " + str(query_ids[qid]))
+        tabs = 0
+        printEingerückt(str(qid) + ":", tabs)
+        tabs += 1
+        for scale in range(3):
+            for worker in range(3):
+                name = (
+                    "tpc_"
+                    + str(query_ids[qid])
+                    + "_"
+                    + str(worker_size[worker])
+                    + "_"
+                    + str(data_scale[scale])
+                    + ".json"
+                )
+                print(name)
+                try:
+                    directory = "tpc"
+                    f = open(os.path.join(directory, name))
+                except:
+                    print("File " + name + " not found.")
+                    continue
+
+                data = json.load(f)
+
+                qTime = transTimes(data["queryStats"]["executionTime"])
+                printEingerückt("ExecutionTime: " + str(qTime), tabs)
+                query_times[worker][scale][qid] = qTime
+
+                qData = convertByteToGB(data["queryStats"]["spilledDataSize"])
+                printEingerückt("Spilled Data: " + str(qData) + " GB", tabs)
+                spilledData[worker][scale][qid] = qData
+
+                # p_user_mem = convertByteToGB(data["queryStats"]["peakUserMemoryReservation"])
+                # printEingerückt("Peak User Memory: " + str(p_user_mem) + " GB", tabs)
+                # query_p_user_mem[worker][qid].append(p_user_mem)
+                # qspillSize[memcounter][threshcounter] = qData
+                # qexecTime[memcounter][threshcounter] = float(qTime[:-1])
+                for i in data["queryStats"]["operatorSummaries"]:
+                    printEingerückt(i["operatorType"], tabs)
+                    if i["operatorType"] == "HashBuilderOperator":
+                        wtime, cputime, spill = CollectOPData(i, tabs + 1)
+
+                    elif i["operatorType"] == "LookupJoinOperator":
+                        wtime, cputime, spill = CollectOPData(i, tabs + 1)
+
+                    elif i["operatorType"] == "HashAggregationOperator":
+                        wtime, cputime, spill = CollectOPData(i, tabs + 1)
+
+                    elif i["operatorType"] == "AggregationOperator":
+                        wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                    elif "Exchange" in i["operatorType"]:
+                        wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                    else:
+                        wtime, cputime, spill = CollectOPData(i, tabs + 1)
+                f.close()
+        tabs -= 1
+
+    for h in range(2):
+        data = query_times if h == 0 else spilledData
+
+        fig, axs = plt.subplots(3, 1)
+        x = np.arange(len(query_ids))
+        width = 0.25  # the width of the bars
+        labels = ["Scale factor 100", "Scale factor 300", "Scale factor 1000"]
+        for k in range(3):
+            ax = axs[k]
+            bottom = np.zeros(len(query_ids))
+            for i in range(3):
+                rects = ax.bar(x + width * i, data[k][i], width, label=labels[i])
+                ax.bar_label(rects, padding=2)
+
+            ax.set_xticks(x + width)
+            ax.set_xticklabels(query_ids)
+            ax.legend(loc="upper right")
+            ax.set_ylabel("Query Execution-time in min")
+        plt.show()
+
+
 TPC()
+# analyse_Query("8")
+analyse_1_6_13()
