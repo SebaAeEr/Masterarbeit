3f88d396cf2a8698cb5f61bf0649f5531b2d56fe
seba <sebthelegend@DESKTOP-6DCQ2BN>
2024-10-16 23:53:48 +0200

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -7,94 +7,74 @@
 #include <string>
 #include <json/json.h>
 #include <unordered_map>
+#include <stdio.h>
 
-int aggregate(std::string inputfilename, std::string outputfilename)
+int parseLine(char *line)
 {
-    int fd = open(inputfilename.c_str(), O_RDONLY);
-    struct stat stats;
-    stat(inputfilename.c_str(), &stats);
-    size_t size = stats.st_size;
-    std::cout << "Input file size: " << stats.st_size << std::endl;
-    void *mappedFile = mmap(nullptr, size, PROT_READ, MAP_SHARED, fd, 0);
-    char *fileChars = static_cast<char *>(mappedFile);
-    Json::CharReaderBuilder readerBuilder;
-    Json::Value inputDataAsJson;
+    // https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
+    //  This assumes that a digit will be found and the line ends in " Kb".
+    int i = strlen(line);
+    const char *p = line;
+    while (*p < '0' || *p > '9')
+        p++;
+    line[i - 3] = '\0';
+    i = atoi(p);
+    return i;
+}
 
-    std::string err;
-    const std::unique_ptr<Json::CharReader> reader(readerBuilder.newCharReader());
-    std::string line = "";
-    std::string ckey, okey;
-    std::unordered_map<std::string, int> hashmap;
-    std::vector<std::string> vectortemp;
-    bool reading = false;
-    for (int i = 0; i < size; ++i)
+int getVirtValue()
+{ // Note: this value is in KB!
+    FILE *file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL)
     {
-        if (fileChars[i] == '{')
+        if (strncmp(line, "VmSize:", 7) == 0)
         {
-            reading = true;
-        }
-        if (reading)
-        {
-            line += fileChars[i];
-            if (fileChars[i] == '}')
-            {
-                if (reader->parse(line.c_str(), line.c_str() + line.length(), &inputDataAsJson, &err))
-                {
-                    ckey = inputDataAsJson["custkey"].asString();
-                    okey = inputDataAsJson["orderkey"].asString();
-                    if (hashmap.contains(ckey))
-                    {
-                        if (!okey.empty())
-                        {
-                            /*  // vectortemp = hashmap[ckey];
-                             hashmap[ckey].push_back(okey);
-                             if (std::find(vectortemp.begin(), vectortemp.end(), okey) == vectortemp.end())
-                             {
-                                 hashmap[ckey].push_back(okey);
-                             } */
-                            hashmap[ckey] += 1;
-                        }
-                    }
-                    else
-                    {
-                        std::pair<std::string, int> pair(ckey, 0);
-                        if (!okey.empty())
-                        {
-                            pair.second = 1;
-                        }
-
-                        hashmap.insert(pair);
-                    }
-                }
-                else
-                {
-                    std::cout << err << "; " << line << std::endl;
-                }
-                vectortemp.clear();
-                line.clear();
-                reading = false;
-            }
+            result = parseLine(line);
+            break;
         }
     }
-    munmap(mappedFile, size);
-    close(fd);
+    fclose(file);
+    return result;
+}
 
-    long unsigned int output_size = 0;
-    for (auto &it : hashmap)
+int getPhyValue()
+{ // Note: this value is in KB!
+    FILE *file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL)
     {
-        std::string temp_line = "{\"custkey\":" + it.first + ",\"_col1\":" + std::to_string(it.second) + "}\n";
+        if (strncmp(line, "VmRSS:", 6) == 0)
+        {
+            result = parseLine(line);
+            break;
+        }
+    }
+    fclose(file);
+    return result;
+}
+
+int writeHashmap(std::unordered_map<int, int> hmap, int file, int start)
+{
+    long unsigned int output_size = 0;
+    for (auto &it : hmap)
+    {
+        std::string temp_line = "{\"custkey\":" + std::to_string(it.first) + ",\"_col1\":" + std::to_string(it.second) + "}\n";
         output_size += strlen(temp_line.c_str());
     }
-    int output_fd = open(outputfilename.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0777);
-    lseek(output_fd, output_size - 1, SEEK_SET);
-    if (write(output_fd, "", 1) == -1)
+    lseek(file, start + output_size - 1, SEEK_SET);
+    if (write(file, "", 1) == -1)
     {
         close(fd);
         perror("Error writing last byte of the file");
         exit(EXIT_FAILURE);
     }
     std::cout << "Output file size: " << output_size << std::endl;
-    char *mappedoutputFile = static_cast<char *>(mmap(nullptr, output_size, PROT_WRITE | PROT_READ, MAP_SHARED, output_fd, 0));
+    char *mappedoutputFile = static_cast<char *>(mmap(nullptr, output_size, PROT_WRITE | PROT_READ, MAP_SHARED, file, 0));
     if (mappedoutputFile == MAP_FAILED)
     {
         close(fd);
@@ -103,9 +83,9 @@ int aggregate(std::string inputfilename, std::string outputfilename)
     }
 
     int mapped_count = 0;
-    for (auto &it : hashmap)
+    for (auto &it : hmap)
     {
-        std::string temp_line = "{\"custkey\":" + it.first + ",\"_col1\":" + std::to_string(it.second) + "}\n";
+        std::string temp_line = "{\"custkey\":" + std::to_string(it.first) + ",\"_col1\":" + std::to_string(it.second) + "}\n";
         for (auto &itt : temp_line)
         {
             mappedoutputFile[mapped_count] = itt;
@@ -120,8 +100,154 @@ int aggregate(std::string inputfilename, std::string outputfilename)
     // memcpy(mappedoutputFile, jsonString.c_str(), output_size);
 
     munmap(mappedoutputFile, output_size);
-    close(output_fd);
-    return 0;
+    return output_size;
+}
+
+int aggregate(std::string inputfilename, std::string outputfilename)
+{
+    int fd = open(inputfilename.c_str(), O_RDONLY);
+    struct stat stats;
+    stat(inputfilename.c_str(), &stats);
+    size_t size = stats.st_size;
+    std::cout << "Input file size: " << stats.st_size << std::endl;
+    char *mappedFile = static_cast<char *>(mmap(nullptr, size, PROT_READ, MAP_SHARED, fd, 0));
+    Json::CharReaderBuilder readerBuilder;
+    Json::Value inputDataAsJson;
+
+    std::string err;
+    const std::unique_ptr<Json::CharReader> reader(readerBuilder.newCharReader());
+    std::string line = "";
+    int ckey;
+    std::string okey;
+    std::unordered_map<int, int> hashmap;
+    std::vector<std::string> vectortemp;
+    bool reading = false;
+    int virtMemBase = getVirtValue();
+    std::cout << "virtMemBase: " << virtMemBase << std::endl;
+    int phyMemBase = getPhyValue();
+    std::cout << "phyMemBase: " << phyMemBase << std::endl;
+    float avg = 0.44;
+    float memLimit = 0.5;
+    int *spill;
+    bool spill_occ = false;
+    int spill_size;
+    std::vector<std::pair<int, int>> spills;
+    for (int i = 0; i < size; ++i)
+    {
+        if (mappedFile[i] == '{')
+        {
+            reading = true;
+        }
+        if (reading)
+        {
+            line += mappedFile[i];
+            if (mappedFile[i] == '}')
+            {
+                if (reader->parse(line.c_str(), line.c_str() + line.length(), &inputDataAsJson, &err))
+                {
+                    ckey = inputDataAsJson["custkey"].asInt();
+                    okey = inputDataAsJson["orderkey"].asString();
+                    if (hashmap.contains(ckey))
+                    {
+                        if (!okey.empty())
+                        {
+                            hashmap[ckey] += 1;
+                        }
+                    }
+                    else
+                    {
+                        std::pair<int, int> pair(ckey, 0);
+                        if (!okey.empty())
+                        {
+                            pair.second = 1;
+                        }
+
+                        hashmap.insert(pair);
+                        if (hashmap.size() % 100000 == 0)
+                        {
+                            int curphyValue = getPhyValue();
+                            avg = (curphyValue - phyMemBase) / (float)(hashmap.size());
+                            std::cout << "total: " << curphyValue << " hashmap size: " << hashmap.size() << " div: " << avg << std::endl;
+                        }
+                        if (hashmap.size() * avg > memLimit * (1ull << 20))
+                        {
+                            avg = (getPhyValue() - phyMemBase) / (float)(hashmap.size());
+                            if (hashmap.size() * avg > memLimit * (1ull << 20))
+                            {
+                                phyMemBase += getPhyValue();
+                                size_t spill_mem_size = hashmap.size() * sizeof(int) * 2;
+                                spill_size = hashmap.size() * 2;
+                                std::string file_name = "spill_" + std::to_string(spills.size()) + outputfilename;
+                                int spill_fd = open(file_name.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0777);
+                                lseek(spill_fd, spill_mem_size - 1, SEEK_SET);
+                                if (write(spill_fd, "", 1) == -1)
+                                {
+                                    close(fd);
+                                    perror("Error writing last byte of the file");
+                                    exit(EXIT_FAILURE);
+                                }
+                                spills.push_back(std::pair<int, int>(spill_fd, spill_size));
+                                spill = (int *)(mmap(nullptr, spill_mem_size, PROT_WRITE | PROT_READ, MAP_SHARED, spill_fd, 0));
+                                if (spill == MAP_FAILED)
+                                {
+                                    close(fd);
+                                    perror("Error mmapping the file");
+                                    exit(EXIT_FAILURE);
+                                }
+                                int counter = 0;
+                                for (auto &it : hashmap)
+                                {
+                                    spill[counter] = it.first;
+                                    counter += 1;
+                                    spill[counter] = it.second;
+                                    counter += 1;
+                                }
+                                hashmap.clear();
+                                spill_occ = true;
+                                std::cout << "Spilled with size: " << spill_mem_size << std::endl;
+                                std::cout << "PhyMem: " << getPhyValue() << std::endl;
+                                munmap(spill, spill_size * sizeof(int));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    std::cout << err << "; " << line << std::endl;
+                }
+                vectortemp.clear();
+                line.clear();
+                reading = false;
+            }
+        }
+    }
+    munmap(mappedFile, size);
+    close(fd);
+    int output_fd = open(outputfilename.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0777);
+    int head = 0;
+    if (spill_occ)
+    {
+        for (auto &spill_file : spills)
+        {
+            int *spill_map = static_cast<int *>(mmap(nullptr, spill_file.second * sizeof(int), PROT_WRITE | PROT_READ, MAP_SHARED, spill_file.first, 0));
+            for (int i = 0; i < spill_file.second; i += 2)
+            {
+                int custkey = spill_map[i];
+                int count = spill_map[i + 1];
+                if (hashmap.contains(custkey))
+                {
+                    hashmap[custkey] = hashmap[custkey] + count;
+                    spill_map[i] = -1;
+                    spill_map[i + 1] = -1;
+                }
+            }
+        }
+        head += writeHashmap(hashmap, output_fd, head);
+    }
+    else
+    {
+        writeHashmap(hashmap, output_fd, 0);
+    }
 }
 
 int test(std::string file1name, std::string file2name)
