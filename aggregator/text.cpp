01f7904cc2e3f6e762ca6eace5d3fab2475456a3
seba <sebthelegend@DESKTOP-6DCQ2BN>
2024-11-27 19:52:04 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -2375,6 +2375,8 @@ int main(int argc, char **argv)
 
     if (co_output != "-")
     {
+        manaFile mana = getLockedMana(&minio_client, 0);
+        writeMana(minio_client, mana, false);
         aggregate(co_output, agg_output, memLimit, true, minio_client, memLimitMain);
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = (float)(std::chrono::duration_cast<std::chrono::microseconds>(stop - start_time).count()) / 1000000;
