b6f9378efd9ec1d2ddf97c05f82decdd083ed989
seba <sebthelegend@DESKTOP-6DCQ2BN>
2024-11-28 12:37:35 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -350,6 +350,7 @@ bool writeMana(Aws::S3::S3Client *minio_client, manaFile mana, bool freeLock, in
         else
         {
             manag_version = in_outcome.GetResult().GetVersionId();
+            PrintLock(minio_client);
             if (freeLock)
             {
                 std::cout << "Lock released by: " << std::to_string((int)(mana.thread_lock)) << std::endl;
