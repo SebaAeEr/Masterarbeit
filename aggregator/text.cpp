e43e58bdbe840f4c5969fb561fa5148d7bfc030f
seba <sebthelegend@DESKTOP-6DCQ2BN>
2024-12-11 17:05:38 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -736,9 +736,18 @@ void getMergeFileName(emhash8::HashMap<std::array<unsigned long, max_size>, std:
                         for (auto &file : partition.files)
                         {
                             std::cout << "File status: " << file.status << " size: " << file.size << " name: " << file.name << std::endl;
-                            if (file.status == 0 && !std::count(blacklist->begin(), blacklist->end(), file.name) && !std::count(res_files.begin(), res_files.end(), file.name))
+                            if (file.status == 0 && !std::count(blacklist->begin(), blacklist->end(), file.name))
                             {
-                                if (file.size > max)
+                                bool found = false;
+                                for (auto &f : res_files)
+                                {
+                                    if (f.name == file.name)
+                                    {
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if (!false && file.size > max)
                                 {
                                     max = file.size;
                                     biggest_file = file;
