c9a99c23a72ffa1fb55bb807dc4df11e301bff93
seba <sebthelegend@DESKTOP-6DCQ2BN>
2024-12-25 14:43:16 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -3180,18 +3180,18 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
         finished = subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, true, &s3spillFile_head, &bit_head, &subfile_head, &s3spillStart_head, &s3spillStart_head_chars, &input_head_base,
                             size_after_init, &read_lines, minio_client, &writeLock, &readNum, avg, memLimit, comb_hash_size, diff, increase);
         increase = false;
-        if (!finished)
-        {
-            size_t n = 0;
-            int int_n = 0;
-            s3spillFile_head++;
-            input_head_base++;
-            subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, false, &s3spillFile_head, &int_n, &int_n, &n, &n, &input_head_base,
-                     size_after_init, &read_lines, minio_client, &writeLock, &readNum, avg, memLimit, comb_hash_size, diff, false);
+        // if (!finished)
+        //{
+        size_t n = 0;
+        int int_n = 0;
+        s3spillFile_head++;
+        input_head_base++;
+        subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, false, &s3spillFile_head, &int_n, &int_n, &n, &n, &input_head_base,
+                 size_after_init, &read_lines, minio_client, &writeLock, &readNum, avg, memLimit, comb_hash_size, diff, false);
 
-            s3spillFile_head--;
-            input_head_base--;
-        }
+        s3spillFile_head--;
+        input_head_base--;
+        //}
 
         if (writeRes)
         {
