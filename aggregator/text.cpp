83a08fd50907e846c38d11edaae91de5ce67e9c5
seba <sebthelegend@DESKTOP-6DCQ2BN>
2024-12-27 16:54:12 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -110,7 +110,7 @@ bool mergePhase = false;
 bool set_partitions = true;
 bool straggler_removal = true;
 bool multiThread_merge = true;
-std::vector<unsigned long> test_values = {};
+std::vector<unsigned long> test_values = {4429504};
 int partitions = -1;
 logFile log_file;
 std::atomic<int> mana_writeThread_num(0);
