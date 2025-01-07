a2f5ecd148b94c67518add1d4c3a782e973d48f5
seba <sebthelegend@DESKTOP-6DCQ2BN>
2025-01-07 16:43:32 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -2559,7 +2559,7 @@ void printSize(int &finished, size_t memLimit, int threadNumber, std::atomic<uns
 bool subMerge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsigned long, max_size>, decltype(hash), decltype(comp)> *hmap,
               std::set<std::tuple<std::string, size_t, std::vector<std::pair<size_t, size_t>>>, CompareBySecond> *s3spillNames2,
               std::vector<std::pair<int, std::vector<char>>> *s3spillBitmaps, std::vector<std::pair<int, size_t>> *spills, bool add, int *s3spillFile_head,
-              int *bit_head, int *bit_head_end, int *subfile_head, size_t *s3spillStart_head, size_t *s3spillStart_head_chars, size_t *input_head_base, size_t size_after_init, std::atomic<size_t> *read_lines,
+              int *bit_head, int *subfile_head, size_t *s3spillStart_head, size_t *s3spillStart_head_chars, size_t *input_head_base, size_t size_after_init, std::atomic<size_t> *read_lines,
               Aws::S3::S3Client *minio_client, std::mutex *writeLock, float *avg, float memLimit, std::atomic<unsigned long> &comb_hash_size,
               std::atomic<unsigned long> *diff, bool increase_size, size_t *max_hash_size, int t_id)
 {
@@ -2595,7 +2595,6 @@ bool subMerge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<u
     std::array<unsigned long, max_size> values = {0, 0};
 
     int bit_i = *bit_head;
-    *bit_head_end = *bit_head;
     if (it_counter < s3spillNames2->size())
     {
         for (auto set_it = std::next(s3spillNames2->begin(), it_counter); set_it != s3spillNames2->end(); set_it++)
@@ -2615,7 +2614,6 @@ bool subMerge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<u
             // std::cout << "Reading " << get<0>(*set_it) << std::endl;
             firsts3File = hmap->empty();
             int sub_file_counter = 0;
-            *bit_head_end = *bit_head_end + get<2>(*set_it).size();
 
             if (firsts3File)
             {
@@ -2636,7 +2634,7 @@ bool subMerge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<u
                 {
                     std::cout << " merging ";
                 }
-                std::cout << get<0>(*set_it) + "_" + std::to_string(sub_file_counter) << " bitmap: " << bit_i << " Read lines: " << read_lines << " bit_head_end: " << *bit_head_end << std::endl;
+                std::cout << get<0>(*set_it) + "_" + std::to_string(sub_file_counter) << " bitmap: " << bit_i << " Read lines: " << read_lines << std::endl;
                 Aws::S3::Model::GetObjectRequest request;
                 request.SetBucket(bucketName);
                 request.SetKey(get<0>(*set_it) + "_" + std::to_string(sub_file_counter));
@@ -3337,7 +3335,6 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
     int s3spillFile_head = 0;
     int subfile_head = 0;
     int bit_head = 0;
-    int bit_head_end = 0;
     unsigned long s3spillStart_head = 0;
     unsigned long s3spillStart_head_chars = 0;
     unsigned long overall_s3spillsize = 0;
@@ -3424,9 +3421,10 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
     while (!finished)
     {
         auto s3spillFile_head_old = s3spillFile_head;
-        finished = subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, true, &s3spillFile_head, &bit_head, &bit_head_end, &subfile_head, &s3spillStart_head, &s3spillStart_head_chars, &input_head_base,
+        finished = subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, true, &s3spillFile_head, &bit_head, &subfile_head, &s3spillStart_head, &s3spillStart_head_chars, &input_head_base,
                             size_after_init, &read_lines, minio_client, &writeLock, avg, memLimit, comb_hash_size, diff, increase, max_hash_size, 0);
-        std::cout << "Start adding from: " << s3spillFile_head_old << " to " << s3spillFile_head << " subfile_head: " << subfile_head << " bit_head_end: " << bit_head_end << std::endl;
+
+        std::cout << "Start adding from: " << s3spillFile_head_old << " to " << s3spillFile_head << " subfile_head: " << subfile_head << std::endl;
         // std::cout << "comb_hash_size: " << comb_hash_size.load() << " max_hash_size: " << *max_hash_size << std::endl;
         // bit_head_end++;
         increase = false;
@@ -3435,8 +3433,17 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
             size_t n = 0;
             int int_n = 0;
             auto temp_it = s3spillNames2->begin();
+            int start_bit_merge = 0;
             s3spillFile_head++;
-            size_t old_input_head_base = input_head_base;
+
+            for (int i = 0; i < s3spillFile_head; i++)
+            {
+                start_bit_merge += get<2>(*temp_it).size();
+                temp_it++;
+            }
+            std::cout << "Start merging from: " << start_bit_merge << std::endl;
+
+                        size_t old_input_head_base = input_head_base;
             addXtoLocalSpillHead(spills, &input_head_base, 1);
 
             // std::cout << "round local spill: " << old_input_head_base << " up to: " << input_head_base << std::endl;
@@ -3468,7 +3475,7 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
                 std::vector<int> start_bits(s3spillNames2->size());
                 std::vector<unsigned long> start_heads_local(spills->size());
                 int s3_start_head = s3spillFile_head;
-                int start_bit_head = bit_head_end;
+                int start_bit_head = start_bit_merge;
                 counter = 0;
                 int t_c = 0;
                 while (s3_start_head < s3spillNames2->size())
@@ -3477,7 +3484,7 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
                     start_heads[counter] = s3_start_head;
                     start_bits[counter] = start_bit_head;
                     // std::cout << "merging s3 start_head: " << s3_start_head << " bit_start_head: " << start_bit_head << std::endl;
-                    threads.push_back(std::thread(subMerge, hmap, s3spillNames2, &s3spillBitmaps, spills, false, &start_heads[counter], &start_bits[counter], &bit_head_end, &int_n, &n, &n, &input_head_base,
+                    threads.push_back(std::thread(subMerge, hmap, s3spillNames2, &s3spillBitmaps, spills, false, &start_heads[counter], &start_bits[counter], &int_n, &n, &n, &input_head_base,
                                                   size_after_init, &read_lines, minio_client, &writeLock, avg, memLimit, std::ref(comb_hash_size), diff, false, max_hash_size, t_c));
                     counter++;
                     t_c++;
@@ -3504,7 +3511,7 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
                 {
                     start_heads_local[counter] = input_head_base;
                     // std::cout << "merging local input_head_base: " << input_head_base << std::endl;
-                    threads.push_back(std::thread(subMerge, hmap, s3spillNames2, &s3spillBitmaps, spills, false, &s3_start_head, &start_bit_head, &bit_head_end, &int_n, &n, &n, &start_heads_local[counter],
+                    threads.push_back(std::thread(subMerge, hmap, s3spillNames2, &s3spillBitmaps, spills, false, &s3_start_head, &start_bit_head, &int_n, &n, &n, &start_heads_local[counter],
                                                   size_after_init, &read_lines, minio_client, &writeLock, avg, memLimit, std::ref(comb_hash_size), diff, false, max_hash_size, t_c));
                     counter++;
                     t_c++;
@@ -3519,7 +3526,7 @@ int merge(emhash8::HashMap<std::array<unsigned long, max_size>, std::array<unsig
             }
             else
             {
-                subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, false, &s3spillFile_head, &bit_head_end, &bit_head_end, &int_n, &n, &n, &input_head_base,
+                subMerge(hmap, s3spillNames2, &s3spillBitmaps, spills, false, &s3spillFile_head, &start_bit_merge, &int_n, &n, &n, &input_head_base,
                          size_after_init, &read_lines, minio_client, &writeLock, avg, memLimit, comb_hash_size, diff, false, max_hash_size, 0);
             }
 
