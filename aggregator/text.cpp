3f30283c1396d35bf674103720013b73fdfcf843
seba <sebthelegend@DESKTOP-6DCQ2BN>
2025-01-22 21:42:33 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -863,12 +863,11 @@ bool writeManaPartition(Aws::S3::S3Client *minio_client, manaFile mana, bool fre
 
     Aws::S3::Model::PutObjectRequest request;
     request.SetBucket(bucketName);
-    request.SetKey(manag_file_name + "_" + worker_id + "_" + partition_id);
+    request.SetKey(manag_file_name + "_" + worker_id + "_" + std::to_string((int)(partition_id)));
     const std::shared_ptr<Aws::IOStream> in_stream = Aws::MakeShared<Aws::StringStream>("");
     *in_stream << "locked";
     request.SetContentLength(6);
     request.SetBody(in_stream);
-    request.SetIfNoneMatch("*");
 
     auto outcome = minio_client->PutObject(request);
     if (!outcome.IsSuccess())
