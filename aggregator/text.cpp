7c0561dca5cb3968ccce208f4b5173f73c668df0
seba <sebthelegend@DESKTOP-6DCQ2BN>
2025-03-09 19:32:47 +0100

    asdf

--- aggregator/text.cpp
+++ aggregator/text.cpp
@@ -3376,7 +3376,7 @@ void fillHashmap(char id, emhash8::HashMap<std::array<unsigned long, max_size>, 
     threadLog threadLog;
     threadLog.sizes["id"] = id;
     threadLog.sizes["inputSize"] = size;
-    thread.sizes["outputLines"] = 0;
+    threadLog.sizes["outputLines"] = 0;
 
     // loop through entire mapping
     for (unsigned long i = 0; i < size + offset; i++)
@@ -5748,7 +5748,8 @@ int aggregate(std::string inputfilename, std::string outputfilename, size_t memL
         w_lines += thread.sizes["outputLines"];
     }
     log_file.sizes["selectivityPostScan"] = (w_lines * 1000) / numLines;
-    if (partitions == -1) {
+    if (partitions == -1)
+    {
         partitions = 1;
     }
 
